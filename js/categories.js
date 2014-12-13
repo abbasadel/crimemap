53c11857451077c78aed9bb71e88cd9d0bc418b5
Abbas Adel <abbas.adel.ibrahim@gmail.com>
2014-12-13 14:29:48 +0100

    update structure and use oop

---
+++ js/categories.js
@@ -0,0 +1,112 @@
+
+function findByAttribute(list, att, value, deep){
+  deep = typeof(deep) !== 'undefined'?deep:false;
+  var found = null;
+  jQuery.each(list, function(index, item){
+    if(value == item[att]){
+      //console.log('found ' + value + ' => ' + item);
+      found = item;
+      return false; //stop loop
+    }
+    if(typeof(item.childs) !== 'undefined' && item.childs.length > 0){
+      found = findByAttribute(item.childs, att, value, deep);
+      if(found != null)
+        return false; //stop
+    }
+
+  });
+
+  if(found == null){
+    //console.log('cant find ' + value);
+  }
+
+  return found;
+}
+
+//state class
+function Category(id, name, color, parentId){
+  this.id = id;
+  this.name = name;
+  this.color = color;
+  this.parentId = parentId;
+  this.parent = null;
+  this.childs = [];
+}
+
+var categories = new function(){
+  this.url = 'json/categories.json';
+  this.data = [];
+
+
+  function assignParent(cat, parents){
+    parent = findByAttribute(parents,'id', cat.parentId);
+
+    if(parent != null){
+      parent.childs.push(cat);
+      cat.parent = parent;
+      return true;
+    }
+    return false;
+  }
+
+
+  // functions
+
+  this.loadData = function(){
+    console.log( this.url + ' loading ');
+    json = $.ajax({
+      url:this.url,
+      dataType:"json",
+      async:false
+    }).responseJSON.payload.categories;
+    console.log( this.url + ' loaded ' + json.length);
+    return json;
+  }
+
+  this.processData = function (data){
+    var parents = [];
+    var later = [];
+
+    jQuery.each(data, function(index, item){
+      var cat = new Category(item.category.id, item.translations.en_US.category_title,
+        item.category.color, item.category.parent_id);
+        if(cat.parentId != 0){
+          //sub category
+          if(!assignParent(cat, parents)){
+            later.push(cat);
+          }
+        }else{
+          //this is a parent category
+          parents.push(cat);
+        }
+    });
+
+    //handle later one last time
+    jQuery.each(later, function(index, item){
+      assignParent(item, parents);
+    });
+
+    console.log(parents);
+    return parents;
+  }
+
+  this.all = function(){
+    return this.data;
+  }
+
+  this.findById = function( id ){
+    return findByAttribute(this.data, 'id', id, true);
+  }
+
+  this.init = function(){
+    console.log('calling init');
+    rawData = this.loadData();
+    this.data = this.processData(rawData);
+    console.log('finish init');
+  }
+
+  /// let's go
+
+  this.init();
+
+};
---
+++ js/states.js
@@ -0,0 +1,97 @@
+var states = new function(){
+  this.url = 'json/states-info.json';
+  this.data = [];
+  this.totalArea = 0;
+  this.totalPopulation = 0;
+
+
+  //state class
+  function State(name, population, area){
+    this.name = name;
+    this.population = population;
+    this.area = area;
+  }
+
+  // functions
+
+  this.loadData = function(){
+    statesInfo = $.ajax({
+      url:this.url,
+      dataType:"json",
+      async:false
+    }).responseJSON;
+    console.log( this.url + ' loaded ' + statesInfo.length);
+    return statesInfo;
+  }
+
+  this.processData = function (data){
+    var states = [];
+
+    jQuery.each(data, function(index, item){
+      states.push(new State(item[0], item[3], item[1]));
+    });
+
+    return states;
+  }
+
+  this.all = function(){
+    return this.data;
+  }
+
+  this.init = function(){
+    console.log('calling init');
+    rawData = this.loadData();
+    this.data = this.processData(rawData);
+    this.calcTotalArea();
+    this.calcTotalPopulation();
+    console.log('finish init');
+  }
+
+  this.findByName = function(name){
+    var found = null;
+    jQuery.each(this.data, function(index, item){
+      if(name == item.name){
+        console.log('found ' + name + ' => ' + item);
+        found = item;
+        return null; //stop loop
+      }
+
+    });
+    if(found == null){
+      console.log('cant find ' + name);
+    }
+
+    return found;
+  }
+
+  this.calcTotalArea = function(){
+    if(this.totalArea != 0) return this.totalArea;
+    var total = 0;
+    jQuery.each(this.data, function(index, item){
+      total += item.area;
+    });
+
+    this.totalArea = total;
+    console.log('totalArea = ' + this.totalArea);
+
+    return this.totalArea;
+  }
+
+  this.calcTotalPopulation = function(){
+    if(this.totalPopulation != 0) return this.totalPopulation;
+    var total = 0;
+    jQuery.each(this.data, function(index, item){
+      total += item.population;
+    });
+
+    this.totalPopulation = total;
+    console.log('totalPopulation = ' + this.totalPopulation);
+
+    return this.totalPopulation;
+  }
+
+  /// let's go
+
+  this.init();
+
+};
